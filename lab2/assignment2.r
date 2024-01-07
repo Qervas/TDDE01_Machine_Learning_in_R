9c90865773d96764db4669c0c47bf21dc1190ac1
Aswathavicky <vigneshaswath67@gmail.com>
2024-01-07 21:04:02 +0100

    Assignment 2 R.file and Report

--- lab2/assignment2.r
+++ lab2/assignment2.r
@@ -1,52 +1,29 @@
 setwd("C:/Masters/TDDE01-Machine Learning/Lab/Lab2")
-library(tidyr)
 library(tree)
 library(rpart)
-bank_data <- read.csv("bank-full.csv")
-column_names <- colnames(bank_data) 
-column_names <- unlist(strsplit(column_names,"[.]")) #fetching column names from bank data
 
-new_bank_data <- separate(bank_data,1, column_names, sep =";") #data formatting
+bank_data=read.csv(file="bank-full.csv",header =TRUE,sep =";")
+bank_data <- bank_data[, !names(bank_data) %in% c("duration")]
 
-# removing column "balance" from data, as not part of input variable
-new_bank_data <- new_bank_data[,-6] 
+for(i in c(2,3,4,5,7,8,9,11,15,16)){
+  bank_data[,i]=as.factor(bank_data[,i])
+}
 
-
-new_bank_data$age <- as.numeric(new_bank_data$age)
-new_bank_data$job <- as.factor(new_bank_data$job)
-new_bank_data$marital <- as.factor(new_bank_data$marital)
-new_bank_data$education <- as.factor(new_bank_data$education)
-new_bank_data$default <- as.factor(new_bank_data$default)
-new_bank_data$housing <- as.factor(new_bank_data$housing)
-new_bank_data$loan <- as.factor(new_bank_data$loan)
-new_bank_data$contact <- as.factor(new_bank_data$contact)
-new_bank_data$month <- as.factor(new_bank_data$month)
-new_bank_data$day <-  as.factor(new_bank_data$day)
-new_bank_data$duration <- as.numeric(new_bank_data$duration)
-new_bank_data$campaign <- as.numeric(new_bank_data$campaign)
-new_bank_data$pdays <- as.numeric(new_bank_data$pdays)
-new_bank_data$previous <- as.numeric(new_bank_data$previous)
-new_bank_data$poutcome <- as.factor(new_bank_data$poutcome)
-new_bank_data$y <- ifelse(new_bank_data$y =="yes", 1,0)
-new_bank_data$y <- as.factor(new_bank_data$y)
-
-#remove input variable "duration"
-processed_bank_data <- new_bank_data[, !names(new_bank_data) %in% c("duration")]
 
 #####Task1#####
 #dividing the data into training/validation/test
-n <- dim(processed_bank_data)[1]
+n <- dim(bank_data)[1]
 set.seed(12345)
 id <- sample(1:n, floor(n*0.4))
-training <- processed_bank_data[id,]
+training <- bank_data[id,]
 
 id1 <- setdiff(1:n, id)
 set.seed(12345)
 id2 <- sample(id1, floor(n*0.3))
-validation <- processed_bank_data[id2,]
+validation <- bank_data[id2,]
 
 id3 <- setdiff(id1,id2)
-test <- processed_bank_data[id3,]
+test <- bank_data[id3,]
 
 
 ####Task2#####
@@ -57,24 +34,24 @@ default_tree <- tree(y~., data = training)
 
 #Prediction with training and validation data(default tree).
 
-prediction_deafult_train = predict(default_tree, training, type = "class")
-prediction_deafult_valid = predict(default_tree, validation, type = "class")
+prediction_deafult_train = predict(default_tree, newdata = training, type = "class")
+prediction_deafult_valid = predict(default_tree, newdata = validation, type = "class")
 
 #B: Decision Tree with smallest allowed node size equal to 7000
 smallest_tree <- tree(y~., data = training, control = tree.control(nobs = nrow(training), minsize = 7000))
 
 #Prediction with training and validation data(Smallest Allowed Node tree).
 
-prediction_smallest_train = predict(smallest_tree, training, type = "class")
-prediction_smallest_valid = predict(smallest_tree, validation, type = "class")
+prediction_smallest_train = predict(smallest_tree, newdata = training, type = "class")
+prediction_smallest_valid = predict(smallest_tree, newdata = validation, type = "class")
 
 #C: Decision Trees minimum deviance to 0.0005.
 mindev_tree <- tree(y~., data = training, control = tree.control(nobs = nrow(training), mindev = 0.0005))
 
 #Prediction with training and validation data(Minimum deviance tree).
 
-prediction_mindev_train = predict(mindev_tree, training, type = "class")
-prediction_mindev_valid = predict(mindev_tree, validation, type = "class")
+prediction_mindev_train = predict(mindev_tree, newdata = training, type = "class")
+prediction_mindev_valid = predict(mindev_tree, newdata = validation, type = "class")
 
 # function to calculate misclassifications rate
 missclass_error <- function(X,Y){
@@ -110,7 +87,7 @@ valid_score <- rep(0,50)
 for(i in 2:50)
 {
   pruned_training <- prune.tree(mindev_tree, best = i)
-  predict_pTree <- predict(pruned_training, validation, type = "tree")
+  predict_pTree <- predict(pruned_training, newdata = validation, type = "tree")
   train_score[i] <- deviance(pruned_training)
   valid_score[i] <- deviance(predict_pTree)
 }
@@ -126,13 +103,14 @@ optimal_tree <- prune.tree(mindev_tree, best = optimal_leaf)
 summary(optimal_tree)
 
 plot(optimal_tree)
+text(optimal_tree, pretty = 1)
 
 
 ####Task4####
 
 # test data prediction with optimal tree
 
-prediction_test <- predict(optimal_tree, test, type = "class" )
+prediction_test <- predict(optimal_tree, newdata = test, type = "class" )
 
 #confusion matrix 
 test_confusionMatrix <- table(true=test$y,prediction_test)
@@ -160,7 +138,7 @@ loss_matrix <- matrix(c(0,5,1,0), nrow = 2)
 
 lost_tree_model <- rpart(y~., data = training, method = "class", parms = list(loss= loss_matrix))
 
-loss_predict_tree <- predict(lost_tree_model, test, type = "class")
+loss_predict_tree <- predict(lost_tree_model, newdata = test, type = "class")
 
 #Confusion Matrix of Tree with Loss Matrix
 loss_confusionMatrix <- table(true=test$y, predicted=loss_predict_tree)
@@ -184,12 +162,12 @@ loss_F1_score
 
 ####Task6####
 
-optimal_final_tree <- prune.tree(mindev_tree, best = 23)
-optimal_tree_predection <- predict(optimal_final_tree, test, type = "vector")
+optimal_final_tree <- prune.tree(mindev_tree, best = optimal_leaf)
+optimal_tree_predection <- predict(optimal_final_tree, newdata = test, type = "vector")
 
 #logistics regression
 logistics <- glm(y~., data = training, family = "binomial")
-logistics_prediction <- predict(logistics, test, type = "response")
+logistics_prediction <- predict(logistics, newdata = test, type = "response")
 
 #computing TPR and FPR with different thresholds
 
@@ -201,8 +179,8 @@ logistics_FPR <- c()
 k <- 1
 
 for(i in seq(0.05, 0.95, 0.05)){
-  t_tree <- ifelse(optimal_tree_predection[,2]>i,1,0)
-  t_logistics <- ifelse(logistics_prediction>i, 1,0)
+  t_tree <- ifelse(optimal_tree_predection[,2]>i,"yes","no")
+  t_logistics <- ifelse(logistics_prediction>i, "yes","no")
   t1 <- table(test$y, t_tree)
   if(dim(t1)[2]>1){
     tree_TPR[k] <- t1[2,2] / sum(t1[2,])
